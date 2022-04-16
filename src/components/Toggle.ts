939d1b0c937096f177712ae3e172d6597a046f6b
Tony Sullivan <tony.f.sullivan@outlook.com>
2022-04-16 14:28:03 +0200

    listening to clicks on hn-toggle directly

--- src/components/Toggle.ts
+++ src/components/Toggle.ts
@@ -1,23 +1,18 @@
 class HnToggle extends HTMLElement {
-    #anchor: HTMLAnchorElement
-
     static get observedAttributes() {
         return ['open']
     }
 
     connectedCallback() {
-        this.#anchor = this.querySelector<HTMLAnchorElement>('a')
-        this.#anchor.addEventListener('click', () => this.open = !this.open)
+        this.addEventListener('click', () => this.open = !this.open)
     }
 
     attributeChangedCallback() {
-        if (!this.#anchor) { return }
-
         if (this.open) {
-            this.#anchor.innerText = '[-]'
+            this.innerText = '[-]'
             this.classList.add('open')
         } else {
-            this.#anchor.innerText = '[+] comments collapsed'
+            this.innerText = '[+] comments collapsed'
             this.classList.remove('open')
         }
     }
